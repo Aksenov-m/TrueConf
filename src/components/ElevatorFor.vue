a1f48f9fad32ebbc35800ebace38f1869f812184
Mikhail <aksenov.m87@yandex.ru>
2023-09-27 06:24:06 +0300

    create architecture

---
+++ src/components/ElevatorFor.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+// import { faElevator } from '@fortawesome/free-solid-svg-icons'
+
+
+</script>
+
+<template>
+    <div>
+        <font-awesome-icon :icon="['fas', 'elevator']" style="color: #511f1f;" />
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ShaftContainer.vue
@@ -0,0 +1,18 @@
+<script setup>
+import config from '../config/default.json'
+
+import ElevatorFor from './ElevatorFor.vue'
+
+const elevator = config.ELEVATOR
+</script>
+
+<template>
+    <div>
+        <ElevatorFor v-for="item in elevator" :key="item"/>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
